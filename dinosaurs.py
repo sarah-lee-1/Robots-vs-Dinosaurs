ff64e8a07ae5b17f480dba001094f5dca82be324
Sarah Yegge <syeg.schreiber@gmail.com>
2021-11-11 11:57:44 -0600

    'Assigned' attributes to Robots and Dinosaurs.

---
+++ dinosaurs.py
@@ -0,0 +1,7 @@
+class Dinosaur:
+    def __init__(self, name)
+        self.name = name
+        self.health = health
+        self.attack_power = attack_power("talon", 5)
+
+    
---
+++ robots.py
@@ -0,0 +1,6 @@
+class Robot:
+    def __init__(self, name)
+        self.name = name 
+        self.health = 100
+        weapon = weapon("sword", 10)
+        
